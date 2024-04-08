93c1e6192c8f9f5e399acfed604a1d5ceaf1ba35
yomna <yomnaothman@aucegypt.edu>
2024-04-08 12:12:09 +0200

    Started Obstacles

--- FireboyWaterGirl/obstacles.h
+++ FireboyWaterGirl/obstacles.h
@@ -1,9 +1,13 @@
 #ifndef OBSTACLES_H
 #define OBSTACLES_H
 
-class Obstacles
-{
+#include<QObject>
+#include<QGraphicsPixmapItem>
+#include<QPixamp>
+class Obstacles : public QObject, public QGraphicsPixmapItem {
+    Q_OBJECT
 public:
+    enum ObstacleType{Pavement, Fire, Water, Acid, Lever, Button, SlidingFloor, FireDoor, WaterDoor};
     Obstacles();
 };
 
