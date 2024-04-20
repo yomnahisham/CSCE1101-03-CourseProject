1a45934f08eb69e4dab266d66a3e64afa5c4a16d
yomna <yomnaothman@aucegypt.edu>
2024-04-20 17:15:45 +0200

    working on keypress to improve animation, need to figure out a way to know the max and the min of each pavement layer in layout

--- FireboyWaterGirl/fireboy.h
+++ FireboyWaterGirl/fireboy.h
@@ -12,6 +12,10 @@ public:
     void keyPressEvent(QKeyEvent* event) override;
     void boundries() override;
     void kill() override;
+    double Pos[2]; // array for position with x and y
+    double Velocity[2]; // velocity array in directions x and y
+    double Acceleration; // acceleration in y (gravity effect)
+
 
 private:
     void jump(int jumpStep, int height = 10);
@@ -19,6 +23,35 @@ private:
     bool right, left = true;
     bool isJumping = false;
     int jumpStep;
+
+    bool keyUpPressed;
+    bool keyRightPressed;
+    bool keyLeftPressed;
 };
 
 #endif // FIREBOY_H
+/*int id1;
+double x[2];
+double y[2];
+double vx[2];
+double vy[2];
+double ay[2];
+double maxx[2];
+double maxy[2];
+double minx[2];
+double miny[2];
+int rednumber,bluenumber;
+bool redget[4];
+bool blueget[4];
+int move_;
+bool getMaxAndMin(int,int,int);
+void reset();
+~game();
+protected:
+void timerEvent(QTimerEvent *event);
+private:
+Ui::game *ui;
+signals:
+void winend1();
+void loseend1();
+void winend2();*/
