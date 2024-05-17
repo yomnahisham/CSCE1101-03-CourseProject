2517c021eacb8f1c246a9d2ec925c69a7769b4f3
yomna <yomnaothman@aucegypt.edu>
2024-05-17 15:33:11 +0300

    level commented

--- FireboyWaterGirl/level.cpp
+++ FireboyWaterGirl/level.cpp
@@ -25,6 +25,7 @@ void Level::on_Level1_clicked()
 
 void Level::on_level2B_clicked()
 {
+    // starts layout, 2
     WindowManager Manager;
     Manager.showWindow(WindowManager::main, 2, user);
     hide();
@@ -33,6 +34,7 @@ void Level::on_level2B_clicked()
 
 void Level::on_level3B_clicked()
 {
+    // starts layout, 3
     WindowManager Manager;
     Manager.showWindow(WindowManager::main, 3, user);
     hide();
@@ -41,6 +43,7 @@ void Level::on_level3B_clicked()
 
 void Level::on_level4B_clicked()
 {
+    // starts layout, 4
     WindowManager Manager;
     Manager.showWindow(WindowManager::main, 4, user);
     hide();
@@ -49,6 +52,7 @@ void Level::on_level4B_clicked()
 
 void Level::on_level5B_clicked()
 {
+    // starts layout, 5
     WindowManager Manager;
     Manager.showWindow(WindowManager::main, 5, user);
     hide();
