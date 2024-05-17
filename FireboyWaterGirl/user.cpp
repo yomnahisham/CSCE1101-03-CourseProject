43629c6e7b6671f566a0b8cd476298306c6d6009
yomna <yomnaothman@aucegypt.edu>
2024-05-17 15:37:51 +0300

    user commented

--- FireboyWaterGirl/user.cpp
+++ FireboyWaterGirl/user.cpp
@@ -3,11 +3,14 @@
 #include <QDebug>
 
 User::User(const QString& username, const QString& password) : username(username), password(password){
-    score = 0;
+    score = 0; // constrcutor- initializes everything and puts initial score to 0
 }
 
+// constructor with different parameter, made for all users authentication
 User::User(const QString& username, const QString& password, const int& s): username(username), password(password), score(s){}
 
+
+// adds new user to the db
 void User::newUser(const QString& username, const QString& password){
     AllUsers::addUser(username, password);
 }
